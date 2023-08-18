25700b895410a9e3dd6b2e2c24bfaeb8350a7267
Impton <142291501+Impton@users.noreply.github.com>
2023-08-18 13:26:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 # set your telegram bot token
-BOT_TOKEN = ""
+BOT_TOKEN = "5595411530:AAEUqKVyZlVQVcJaxLbFaXg0kxXlTa_IVIY"
 tutorial = "BAACAgIAAxkBAAIGQmPrxt24nrWaOytCgu-kUDcUHLseAAK7HwACMh1hS9ol-KxNRCjCLgQ"
